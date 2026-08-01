32046af21a243c64e7cbdaa7445256c01dc0fd84
ARYAN GARG <aryangarg075@gmail.com>
2026-08-01 22:53:16 +0530

    Time: 328 ms (14.98%), Space: 19.2 MB (56.66%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,11 +1,25 @@
+# class Solution:
+#     def fib(self, n: int) -> int:
+        
+#         if n == 0:
+#             return 0
+#         if n == 1:
+#             return 1
+        
+        
+#         return self.fib(n - 1) + self.fib(n - 2)
 class Solution:
+    def func(self , num):
+        if num ==0 or num==1:
+            return num
+
+        return self.func(num-1) + self.func(num-2)
+
     def fib(self, n: int) -> int:
+        return self.func(n)
         
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
+
+         
         
-        
-        return self.fib(n - 1) + self.fib(n - 2)
+       
         
