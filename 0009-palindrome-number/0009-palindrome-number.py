826082eb2db239b25fcef5ae1bc18e1e4230960f
ARYAN GARG <aryangarg075@gmail.com>
2026-08-01 13:02:50 +0530

    Time: 17 ms (25.04%), Space: 12.4 MB (55.12%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        num =x
+        result=0
+
+        while num > 0:
+            last_digit=num%10
+            result=result*10+last_digit
+            num = num//10
+
+        return x==result
+
+
+        
+
+
+        
